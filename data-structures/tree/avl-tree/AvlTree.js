c2dca9cc7a873cf321c6926bf1aca536b6ef55f5
KimKyungJi <kkj2551@naver.com>
2021-10-30 08:49:17 +0900

    [feat] avl 트리 코드 작성

---
+++ data-structures/tree/avl-tree/AvlTree.js
@@ -0,0 +1,107 @@
+import BinarySearchTree from "../binary-search-tree/BinarySearchTree";
+
+export default class AvlTree extends BinarySearchTree {
+  insert(value) {
+    super.insert(value);
+
+    let currentNode = this.root.find(value);
+    while (currentNode) {
+      this.balance(currentNode);
+      currentNode = currentNode.parent;
+    }
+  }
+
+  remove(value) {
+    super.remove(value);
+
+    this.balance(this.root);
+  }
+
+  balance(node) {
+    if (node.balanceFactor > 1) {
+      if (node.left.balanceFactor > 0) {
+        this.rotateLeftLeft(node);
+      } else if (node.left.balanceFactor < 0) {
+        this.rotateLeftRight(node);
+      }
+    } else if (node.balanceFactor < -1) {
+      if (node.right.balanceFactor < 0) {
+        this.rotateRightRight(node);
+      } else if (node.right.balanceFactor > 0) {
+        this.rotateRightLeft(node);
+      }
+    }
+  }
+
+  rotateLeftLeft(rootNode) {
+    const leftNode = rootNode.left;
+    rootNode.setLeft(null);
+
+    if (rootNode.parent) {
+      rootNode.parent.setLeft(leftNode);
+    } else if (rootNode === this.root) {
+      this.root = leftNode;
+    }
+
+    if (leftNode.right) {
+      rootNode.setLeft(leftNode.right);
+    }
+
+    leftNode.setRight(rootNode);
+  }
+
+  rotateLeftRight(rootNode) {
+    const leftNode = rootNode.left;
+    rootNode.setLeft(null);
+
+    const leftRightNode = leftNode.right;
+    leftNode.setRight(null);
+
+    if (leftRightNode.left) {
+      leftNode.setRight(leftRightNode.left);
+      leftRightNode.setLeft(null);
+    }
+
+    rootNode.setLeft(leftRightNode);
+
+    leftRightNode.setLeft(leftNode);
+
+    this.rotateLeftLeft(rootNode);
+  }
+
+  rotateRightLeft(rootNode) {
+    const rightNode = rootNode.right;
+    rootNode.setRight(null);
+
+    const rightLeftNode = rightNode.left;
+    rightNode.setLeft(null);
+
+    if (rightLeftNode.right) {
+      rightNode.setLeft(rightLeftNode.right);
+      rightLeftNode.setRight(null);
+    }
+
+    rootNode.setRight(rightLeftNode);
+
+    rightLeftNode.setRight(rightNode);
+
+    this.rotateRightRight(rootNode);
+  }
+
+  rotateRightRight(rootNode) {
+    const rightNode = rootNode.right;
+    rootNode.setRight(null);
+
+    if (rootNode.parent) {
+      rootNode.parent.setRight(rightNode);
+    } else if (rootNode === this.root) {
+      this.root = rightNode;
+    }
+
+    if (rightNode.left) {
+      rootNode.setRight(rightNode.left);
+    }
+
+    rightNode.setLeft(rootNode);
+  }
+}
