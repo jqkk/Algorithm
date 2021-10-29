c568d4e1edf6fe62a1e52415e8676efe41fe80e0
KimKyungJi <kkj2551@naver.com>
2021-10-29 18:04:29 +0900

    [feat] 이진 탐색 트리 테스트 코드 작성(완료)

--- data-structures/tree/binary-search-tree/BinarySearchTree.test.js
+++ data-structures/tree/binary-search-tree/BinarySearchTree.test.js
@@ -30,5 +30,36 @@ describe("BinarySearchTree", () => {
     bst.insert(5);
 
     expect(bst.toString()).toBe("5,10,20");
+
+    const removed1 = bst.remove(5);
+    expect(bst.toString()).toBe("10,20");
+    expect(removed1).toBe(true);
+  });
+
+  it("contain test", () => {
+    const bst = new BinarySearchTree();
+
+    bst.insert(10);
+    bst.insert(20);
+    bst.insert(5);
+
+    expect(bst.contains(20)).toBe(true);
+    expect(bst.contains(40)).toBe(false);
+  });
+
+  it("sorting test", () => {
+    const bst = new BinarySearchTree();
+
+    bst.insert(2);
+    bst.insert(5);
+    bst.insert(4);
+    bst.insert(6);
+    bst.insert(9);
+    bst.insert(7);
+    bst.insert(1);
+    bst.insert(3);
+    bst.insert(8);
+
+    expect(bst.toString()).toBe("1,2,3,4,5,6,7,8,9");
   });
 });
