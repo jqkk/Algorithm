822e31f65e7727c0d3ead26918feec088058e949
KimKyungJi <kkj2551@naver.com>
2021-11-09 13:39:36 +0900

    [chore] RedBlack 트리 보류

---
+++ data-structures/tree/red-black-tree/RedBlackTree.test.js
@@ -0,0 +1,10 @@
+import RedBlackTree from "./RedBlackTree";
+
+describe("RedBlackTree", () => {
+  it("insert", () => {
+    const tree = new RedBlackTree();
+
+    const firstInsertedNode = tree.insert(10);
+    //보류
+  });
+});
